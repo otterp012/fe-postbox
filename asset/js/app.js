9c83cba7ad9127103e88812d0663ac5957063866
otterp012 <87624756+otterp012@users.noreply.github.com>
2022-03-10 13:12:15 +0900

    [otter & J] Step1 (#13)
    * docs: test.md
    
    * docs: test.md 내용 추가
    
    * docs: css.css, index.html 파일 생성
    
    * feat: index.html 기본 town-map tag 작성
    
    * style: reset 스타일 설정
    
    * style: css.css 추가
    
    * feat: reset.css link 추가
    
    * docs: app.js 추가
    
    * docs: gitignore 추가 DS.store ingnored
    
    * feat : town generator 진행중
    
    * feat: querySelector 구현 및 렌더 알고리즘 구현
    
    * fix: towns 삭제
    
    * style: 마을 스타일 변경
    
    * refactor: 함수 class로 변경
    
    * docs: test.MD 삭제
    
    * docs: README.md 내용 수정
    
    * style: bigtown, town 스타일 수정
    
    * chore: renderBigTown, renderTown에 클래스 매개변수 추가
    
    * feat: 셀렉터 함수 오류 수정, 마을을 랜덤한 모양으로 그리기 위한 place-self 속성 랜덤삽입 기능 추가
    
    * style: grid-gap 속성 추가
    
    * chore: setLocation -> getLocation으로 이름 수정
    
    * feat: 우체통 추가하기
    
    * style: grid-template 수정
    
    Co-authored-by: ju-kkim <wndus8198@naver.com>

---
+++ asset/js/app.js
@@ -0,0 +1,157 @@
+class Selectors {
+  constructor() {}
+
+  static IDSelector(str, start = document.body) {
+    const queue = [start];
+    const visited = {};
+    visited[start] = true;
+    while (queue.length) {
+      for (let i = 0; i < queue.length; i++) {
+        let parent = queue.shift();
+        for (let child of parent.childNodes) {
+          if (child.id == str) return child;
+          else {
+            visited[child] = true;
+            queue.push(child);
+          }
+        }
+      }
+    }
+  }
+
+  static AllclassSelector(str) {
+    const start = document.body;
+    const queue = [start];
+    const visited = {};
+    const result = [];
+    visited[start] = true;
+
+    while (queue.length) {
+      for (let i = 0; i < queue.length; i++) {
+        let parent = queue.shift();
+        for (let child of parent.childNodes) {
+          if (String(child.className).indexOf(str) !== -1) result.push(child);
+          else {
+            visited[child] = true;
+            queue.push(child);
+          }
+        }
+      }
+    }
+    return result;
+  }
+
+  static classSelector(str) {
+    const start = document.body;
+    const queue = [start];
+    const visited = {};
+    visited[start] = true;
+
+    while (queue.length) {
+      for (let i = 0; i < queue.length; i++) {
+        let parent = queue.shift();
+        for (let child of parent.childNodes) {
+          if (String(child.className).indexOf(str) !== -1) return child;
+          else {
+            visited[child] = true;
+            queue.push(child);
+          }
+        }
+      }
+    }
+  }
+}
+
+class TownGenerator {
+  constructor(maxTownCount, bigTownCount) {
+    this.maxTownCount = maxTownCount;
+    this.bigTownCount = bigTownCount;
+    this.count = null;
+    this.set = null;
+    this.init();
+  }
+
+  init() {
+    this.renderBigTown();
+    this.renderSmallTown();
+    this.renderPost(4);
+  }
+
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max + 1 - min)) + min;
+  }
+
+  renderBigTown() {
+    const bigTownNum = this.getRandomInt(1, this.bigTownCount);
+    this.count = bigTownNum;
+
+    let html = '';
+    for (let i = 1; i <= bigTownNum; i++) {
+      html += this.renderTown(i, 'bigTowns');
+    }
+
+    Selectors.classSelector('town-container').innerHTML = html;
+    Selectors.AllclassSelector('bigTowns').forEach((bigTown) => {
+      bigTown.style.placeSelf = this.getLocation();
+    });
+  }
+
+  renderSmallTown() {
+    while (this.count <= this.maxTownCount) {
+      let rand = this.getRandomInt(1, this.count);
+      this.count++;
+      Selectors.IDSelector(rand).innerHTML += this.renderTown(this.count);
+    }
+  }
+
+  getLocation() {
+    const LOCATION_ATTR = [
+      'auto center',
+      'normal start',
+      'center normal',
+      'start auto',
+      'end normal',
+      'self-start auto',
+      'self-end normal',
+      'flex-start auto',
+      'flex-end normal',
+      'left auto',
+      'right normal',
+      'baseline normal',
+      'first baseline auto',
+      'last baseline normal',
+      'stretch auto',
+    ];
+    const LOCATION_ATTR_LEN = LOCATION_ATTR.length;
+    return LOCATION_ATTR[this.getRandomInt(0, LOCATION_ATTR_LEN)];
+  }
+
+  getPostCount(number) {
+    const set = new Set();
+    for (let i = 0; i < number; i++) {
+      const randomId = this.getRandomInt(1, this.maxTownCount);
+      const postSize = i;
+      set.add([randomId, postSize]);
+    }
+    return set;
+  }
+
+  renderTown(id, className = '') {
+    return `<div class="towns ${className}" id="${id}">${id}</div>`;
+  }
+
+  renderPost(number) {
+    this.set = this.getPostCount(number);
+    this.set.forEach((arr) => {
+      Selectors.IDSelector(arr[0]).insertAdjacentText('afterbegin', '📮 ');
+    });
+  }
+}
+
+(function () {
+  const townMaxCount = 10;
+  const bigTownCount = 4;
+  const town = new TownGenerator(townMaxCount, bigTownCount);
+})();
